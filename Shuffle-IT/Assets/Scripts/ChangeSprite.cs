5e44e481c4849f483b7b3498e350447bbec07b68
Chelsea Garcia <72681344+chelseagarcia0@users.noreply.github.com>
2021-06-09 09:59:41 +0800

    Add files via upload

---
+++ Shuffle-IT/Assets/Scripts/ChangeSprite.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeSprite : MonoBehaviour
+{
+    private SpriteRenderer rend;
+    private Sprite firstTextbox, secondTextbox, thirdTextbox, fourthTextbox;
+    
+    private void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        
+        firstTextbox = Resources.Load<Sprite>("lvl 1 bety");
+        secondTextbox = Resources.Load<Sprite>("lvl 1 etby");
+        thirdTextbox = Resources.Load<Sprite>("lvl 1 teyb");
+        fourthTextbox = Resources.Load<Sprite>("lvl 1 ybet");
+        rend.sprite = firstTextbox;
+    }
+
+    private void Update()
+    {
+        if (rend.sprite == firstTextbox)
+        {
+            ChangeImage();
+            if (rend.sprite == secondTextbox)
+            {
+                ChangeImage();
+                if (rend.sprite == thirdTextbox)
+                {
+                    ChangeImage();
+                    if (rend.sprite == fourthTextbox)
+                    {
+                        ChangeImage();
+                    }
+                }
+            }
+        }
+    }
+
+    public void ChangeImage()
+    {
+        if (rend.sprite == firstTextbox)
+        {
+            rend.sprite = secondTextbox;
+        }
+        else if (rend.sprite == secondTextbox)
+        {
+            rend.sprite = thirdTextbox;
+        }
+        else if (rend.sprite == thirdTextbox)
+        {
+            rend.sprite = fourthTextbox;
+        }
+        else if (rend.sprite == fourthTextbox)
+        {
+            rend.sprite = firstTextbox;
+        }
+    }
+}
+
